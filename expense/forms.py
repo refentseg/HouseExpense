5208f3e0fae78d1a6e19d931d90575da28c384c1
refentseg <refentsegaonnwe@gmail.com>
2025-04-04 18:23:25 +0200

    Cleaning up some code

--- expense/forms.py
+++ expense/forms.py
@@ -10,8 +10,12 @@ class ExpenseForm(forms.ModelForm):
         model = Expense
         fields = ['category', 'title', 'amount', 'paid_date']
         widgets = {
-            'paid_date': forms.DateInput(attrs={'class':'form-control','type': 'date'}),
+            'paid_date': forms.DateInput(attrs={'class': 'form-control',
+                                                'type': 'date'}),
             'category': forms.Select(attrs={'class': 'form-select'}),
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
-            'amount': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Amount', 'step': '0.01'}),
-}
+            'title': forms.TextInput(attrs={'class': 'form-control',
+                                            'placeholder': 'Title'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control',
+                                               'placeholder': 'Amount',
+                                               'step': '0.01'})
+        }
--- expense/models.py
+++ expense/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 
-# Create your models here. 
+# Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=150, unique=True)
+
     class Meta:
-        verbose_name_plural = "Categories"
-        
+        verbose_name_plural = "Categories" 
+
     def __str__(self):
         return self.name
+
 
 class Expense(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    paid_date = models.DateField('date paid')
-    
+    paid_date = models.DateField('date paid') 
 
     def __str__(self):
         return f"{self.amount} on {self.paid_date}"
